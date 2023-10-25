7cc846d7f9eadb1934802a6a57652f9dde6b00b5
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2023-10-25 21:55:46 +0100

    feat: header update and tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+// External dependencies
 import cloudflare from "@astrojs/cloudflare";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://samuh.dev',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
   adapter: cloudflare()
 });
